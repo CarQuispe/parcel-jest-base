54c1d010d37c6239ce72d39050dd46db5c7387be
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-08-20 00:56:09 -0400

    el 5 no es bisiesto

--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -2,9 +2,15 @@ import esBisiesto from "./esBisiesto";
 
 describe("Es Bisiesto", () => {
   it("El 4 deberia considerarse como anio bisiesto", () => {
-   const result = esBisiesto(4);
-   expect(result).toEqual(true);
+   
+   expect(esBisiesto(4)).toEqual(true);
     
   });
 });
 
+describe("No es  Bisiesto", () => {
+  it("El 5  deberia considerarse como anio bisiesto", () => {
+      expect(esBisiesto(5)).toEqual(false);
+    
+  });
+});
