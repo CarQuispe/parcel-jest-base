76923b569f37be7068aa8387fd9ec3b12da542a5
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-08-28 16:58:23 -0400

    Seleccionar  y mostrar impuesto de estado

--- src/presenter.js
+++ src/presenter.js
@@ -1,22 +1,30 @@
 import Calcular from "./totalizador";
 
 const precioInput = document.querySelector("#precio-input");
-const estadoSelect = document.querySelector("#estado-select"); // Seleccionar el select del estado
-
+const estadoSelect = document.querySelector("#estado--select"); 
 const form = document.getElementById("totalizador-form");
 const resultadoDiv = document.getElementById("resultado-div");
+
+const impuestosPorEstado = {
+  "6.65": "UT",
+  "8": "NV",
+  "6.25": "TX",
+  "4": "AL",
+  "8.25": "CA"
+};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   
   const precio = parseFloat(precioInput.value);
-  const estadoSeleccionado = estadoSelect.value; 
+  const estadoSeleccionado = estadoSelect.value;
   
   if (!isNaN(precio)) {
     resultadoDiv.innerHTML = "<p>Precio ingresado: $" + precio.toFixed(2) + "</p>";
     
     if (estadoSeleccionado) {
-      resultadoDiv.innerHTML += "<p>Estado seleccionado: " + estadoSeleccionado + "</p>"; 
+      const impuesto = impuestosPorEstado[estadoSeleccionado];
+      resultadoDiv.innerHTML += "<p>Estado seleccionado: " + impuesto + " - Impuesto: " + estadoSeleccionado + "%</p>";
     } else {
       resultadoDiv.innerHTML += "<p>Por favor, selecciona un estado</p>";
     }
