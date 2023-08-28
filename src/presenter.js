9eabe0174424d819ebdfe37b6b54bd238b21c66f
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-08-28 16:50:13 -0400

    seleccionarEstado

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import Calcular from "./totalizador";
 
-const precioInput = document.querySelector("#precio-input"); 
+const precioInput = document.querySelector("#precio-input");
+const estadoSelect = document.querySelector("#estado-select"); // Seleccionar el select del estado
 
 const form = document.getElementById("totalizador-form");
 const resultadoDiv = document.getElementById("resultado-div");
@@ -8,12 +9,20 @@ const resultadoDiv = document.getElementById("resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   
-  const precio = parseFloat(precioInput.value); 
+  const precio = parseFloat(precioInput.value);
+  const estadoSeleccionado = estadoSelect.value; 
   
   if (!isNaN(precio)) {
-    resultadoDiv.innerHTML = "<p>Precio ingresado: $" + precio.toFixed(2) + "</p>"; 
+    resultadoDiv.innerHTML = "<p>Precio ingresado: $" + precio.toFixed(2) + "</p>";
+    
+    if (estadoSeleccionado) {
+      resultadoDiv.innerHTML += "<p>Estado seleccionado: " + estadoSeleccionado + "</p>"; 
+    } else {
+      resultadoDiv.innerHTML += "<p>Por favor, selecciona un estado</p>";
+    }
   } else {
     resultadoDiv.innerHTML = "<p>Por favor, ingresa un precio válido</p>";
   }
 });
+
 
