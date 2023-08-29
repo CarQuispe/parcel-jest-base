460a5ce034381fc8ee5c0058ca955db861cd8f15
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-08-28 22:56:10 -0400

    corregido1

--- src/presenter.js
+++ src/presenter.js
@@ -1,36 +1,19 @@
 import Calcular from "./totalizador";
 
-const precioInput = document.querySelector("#precio-input");
-const estadoSelect = document.querySelector("#estado--select"); 
-const form = document.getElementById("totalizador-form");
-const resultadoDiv = document.getElementById("resultado-div");
-
-const impuestosPorEstado = {
-  "6.65": "UT",
-  "8": "NV",
-  "6.25": "TX",
-  "4": "AL",
-  "8.25": "CA"
-};
+const cantidad = document.querySelector("#cantidad-input");
+const precio = document.querySelector("#precio-input");
+const estadoSelect = document.querySelector("#estado--select");
+const form = document.querySelector("#totalizador-form");
+const resultadoDiv = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
-  const precio = parseFloat(precioInput.value);
-  const estadoSeleccionado = estadoSelect.value;
-  
-  if (!isNaN(precio)) {
-    resultadoDiv.innerHTML = "<p>Precio ingresado: $" + precio.toFixed(2) + "</p>";
-    
-    if (estadoSeleccionado) {
-      const impuesto = impuestosPorEstado[estadoSeleccionado];
-      resultadoDiv.innerHTML += "<p>Estado seleccionado: " + impuesto + " - Impuesto: " + estadoSeleccionado + "%</p>";
-    } else {
-      resultadoDiv.innerHTML += "<p>Por favor, selecciona un estado</p>";
-    }
-  } else {
-    resultadoDiv.innerHTML = "<p>Por favor, ingresa un precio válido</p>";
-  }
-});
 
+  const cant = Number.parseInt(cantidad.value);
+  const prec = Number.parseInt(precio.value);
+  const estado = estadoSelect.value;
+  const total = Calcular(cant,prec,parseFloat(estado))
+  resultadoDiv.innerHTML = "<p>Cantidad:  " + cant + "</p>" + "<p> Precio:  "+ prec+"</p>"
+  + "<p> Tasa de impuesto:  "+ estado+"</p>" + "<p> Precio Neto:  "+ total+"</p>";
+});
 
